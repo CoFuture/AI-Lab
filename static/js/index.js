8d5da37b00679df63ecf8dbb3058806c01724092
ningbao <957926787@qq.com>
2020-09-10 16:35:45 +0800

    新增池化层、文本和层间连接

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,12 @@
 import * as THREE from "./libs/three.module.js"
 import {OrbitControls} from "./libs/OrbitControls.js";
-import {addConvLayer, addNeuralLayer, addPoolLayer} from "./nn/tensor.js";
+import {addConnection, addConvLayer, addNeuralLayer, addPoolLayer} from "./nn/tensor.js";
 
 
 let canvas, camera, controls, scene, renderer;
+
+//全局的神经元层的id
+let layer_id = 1;
 
 init();
 // animate();
@@ -21,7 +24,7 @@ function init() {
     renderer.setSize(window.innerWidth, window.innerHeight);
 
     camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
-    camera.position.set(-50, 60, 100);
+    camera.position.set(-100, 60, 180);
 
     // controls
 
@@ -34,28 +37,61 @@ function init() {
 
     controls.maxPolarAngle = Math.PI / 2;
 
-    //添加神经网络层
-    addNeuralLayer(scene, 28, 28);
-
-    //添加卷积层
-    addConvLayer(scene, 3, 3, 1, 1, 15 );
-
-    //添加池化层
-    addPoolLayer(scene, 2, 2, 0, "max", 30);
     // lights
 
     let light1 = new THREE.DirectionalLight(0xffffff);
     light1.position.set(1, 1, 1);
     scene.add(light1);
 
-    let light2 = new THREE.DirectionalLight(0x002288);
+    let light2 = new THREE.DirectionalLight(	0xAFEEEE);
     light2.position.set(-1, -1, -1);
     scene.add(light2);
 
     let light3 = new THREE.AmbientLight(0x222222);
     scene.add(light3);
 
-    //
+    //添加神经网络层
+    let input_layer_id =  addNeuralLayer(scene, 28, 28, 1);
+
+    //添加卷积层
+    let conv_layer1_id = addConvLayer(scene, 3, 3, 25, 1, 20);
+
+    //添加神经层
+    let neural_layer1_id = addNeuralLayer(scene, 26, 26, 25, 40);
+
+    addConnection(scene, neural_layer1_id, conv_layer1_id);
+
+    //添加池化层
+    let pool_layer1_id = addPoolLayer(scene, 2, 2, 0, "max", 60);
+
+
+    //添加神经层
+    let neural_layer2_id = addNeuralLayer(scene, 13, 13, 25, 80);
+
+    //添加卷积层
+    let conv_layer2_id = addConvLayer(scene, 4, 4, 50, 1, 100);
+
+    //添加神经层
+    let neural_layer3_id = addNeuralLayer(scene, 10, 10, 50, 120);
+
+    addConnection(scene, neural_layer3_id, conv_layer2_id);
+
+    //添加池化层
+    let pool_layer2_id = addPoolLayer(scene, 2, 2, 0, "max", 140);
+
+    //添加神经层                                                       
+    let neural_layer4_id = addNeuralLayer(scene, 5, 5, 50, 160);
+
+    //添加全连接层
+    let fully_layer1_id = addNeuralLayer(scene, 32, 32, 1, 180, 1);
+    addConnection(scene, neural_layer4_id, fully_layer1_id);
+
+    let fully_layer2_id = addNeuralLayer(scene, 16, 16, 1, 200, 0);
+    addConnection(scene, fully_layer1_id, fully_layer2_id);
+
+    let fully_layer3_id = addNeuralLayer(scene, 47, 1, 1, 220, 1);
+    addConnection(scene, fully_layer2_id, fully_layer3_id);
+    
 
     window.addEventListener('resize', onWindowResize, false);
 
--- static/js/nn/tensor.js
+++ static/js/nn/tensor.js
@@ -2,7 +2,7 @@
 import * as THREE from "../libs/three.module.js"
 
 //添加神经元隐藏层
-function addNeuralLayer(scene, dimensionX, dimensionY, axisPosition = 0, layerType = 0) {
+function addNeuralLayer(scene, dimensionX, dimensionY, channels = 1, axisPosition = 0, layerType = 0) {
 
     const geometry = new THREE.SphereBufferGeometry(0.5, 16, 16);
     let material;
@@ -10,7 +10,7 @@ function addNeuralLayer(scene, dimensionX, dimensionY, axisPosition = 0, layerTy
     if (layerType === 0)
         material = new THREE.MeshBasicMaterial({color: 0x0099FF});
     else
-        material = new THREE.MeshBasicMaterial({color: 0x33FF00});
+        material = new THREE.MeshBasicMaterial({color: 0x8FBC8F});
 
     const division = 2;       //神经元间距
 
@@ -25,6 +25,9 @@ function addNeuralLayer(scene, dimensionX, dimensionY, axisPosition = 0, layerTy
     let layer_mesh = new THREE.InstancedMesh(geometry, material, neural_count);
     scene.add(layer_mesh);
 
+    //建立位置缓存
+    layer_mesh.instancePosition = [];
+
     //实例化神经元
     let neural_node = new THREE.Object3D();
     //神经元矩阵索引
@@ -37,6 +40,9 @@ function addNeuralLayer(scene, dimensionX, dimensionY, axisPosition = 0, layerTy
             neural_node.position.set(position_x, position_y, axisPosition);
             neural_node.updateMatrix();
 
+            //写入position缓存
+            layer_mesh.instancePosition.push(new THREE.Vector3(position_x, position_y, axisPosition));
+
             layer_mesh.setMatrixAt(matrix_index++, neural_node.matrix);
         }
     }
@@ -44,10 +50,27 @@ function addNeuralLayer(scene, dimensionX, dimensionY, axisPosition = 0, layerTy
     //添加辅助框
     let layer_box = new THREE.Box3();
     layer_box.setFromCenterAndSize(new THREE.Vector3(0, 0, axisPosition),
-        new THREE.Vector3(dimensionX * division + 5, dimensionY * division + 5, 4));
+        new THREE.Vector3(dimensionX * division + 3, dimensionY * division + 3, 4));
 
     let helper = new THREE.Box3Helper(layer_box, 0x000000);
     scene.add(helper);
+
+    let text_position = {
+        x: 0,
+        y: (dimensionY / 2 + 4) * division,
+        z: axisPosition
+    };
+
+    let text = {
+        title: "神经元层",
+        text_dimension: String(dimensionX) + "*" + String(dimensionY)
+            + "*" + String(channels)
+    }
+
+    addSpriteText(scene, text, text_position, 0);
+
+    //返回神经元层的id
+    return layer_mesh.id;
 }
 
 //添加卷积层
@@ -55,15 +78,19 @@ function addNeuralLayer(scene, dimensionX, dimensionY, axisPosition = 0, layerTy
 function addConvLayer(scene, dimensionX, dimensionY, channels = 1,
                       stride = 1, axisPosition = 0, layerType = 0) {
 
-    const geometry = new THREE.PlaneBufferGeometry(6, 6, 1, 1);
-    let material;
+    let material, geometry;
+    if (channels === 1)
+        geometry = new THREE.PlaneBufferGeometry(6, 6, 1, 1);
+    else
+        geometry = new THREE.BoxBufferGeometry(6,6, Math.min(channels, 3));
+
 
     if (layerType === 0)
         material = new THREE.MeshBasicMaterial({color: 0x5F9EA0, side: THREE.DoubleSide});
     else
         material = new THREE.MeshBasicMaterial({color: 0x3CB371, side: THREE.DoubleSide});
 
-    const division = 7;       //神经元间距
+    const division = Math.min(6 + channels, 9);       //神经元间距
 
     //计算神经元总数
     let kernel_count = dimensionX * dimensionY;
@@ -76,6 +103,9 @@ function addConvLayer(scene, dimensionX, dimensionY, channels = 1,
     let kernel_mesh = new THREE.InstancedMesh(geometry, material, kernel_count);
     scene.add(kernel_mesh);
 
+    //建立位置缓存
+    kernel_mesh.instancePosition = [];
+
     //实例化神经元
     let kernel_node = new THREE.Object3D();
     //神经元矩阵索引
@@ -87,23 +117,44 @@ function addConvLayer(scene, dimensionX, dimensionY, channels = 1,
             let position_y = -division * j + y_bias;
             kernel_node.position.set(position_x, position_y, axisPosition);
             kernel_node.updateMatrix();
+
+            kernel_mesh.instancePosition.push(new THREE.Vector3(position_x, position_y, axisPosition));
+
             kernel_mesh.setMatrixAt(matrix_index++, kernel_node.matrix);
         }
     }
 
     //添加辅助框
     let layer_box = new THREE.Box3();
-    layer_box.setFromCenterAndSize(new THREE.Vector3(0, 0, axisPosition),
-        new THREE.Vector3(dimensionX * division, dimensionY * division, 4));
+    let layer_box_center = new THREE.Vector3(0, 0, axisPosition);
+    let layer_box_size = new THREE.Vector3(dimensionX * division, dimensionY * division, Math.min(channels, 3) + 4);
 
+    layer_box.setFromCenterAndSize(layer_box_center, layer_box_size);
     let helper = new THREE.Box3Helper(layer_box, 0x000000);
     scene.add(helper);
 
+    let text_position = {
+        x: 0,
+        y: (dimensionY / 2 + 1) * division,
+        z: axisPosition
+    };
+
+    let text = {
+        title: "卷积层",
+        text_dimension: String(dimensionX) + "*" + String(dimensionY)
+            + "*" + String(channels)
+    }
+
+    //添加文字标识
+    addSpriteText(scene, text, text_position, 1);
+
+    //返回卷积层id
+    return kernel_mesh.id;
 }
 
 //添加池化层
 //参数 场景，维度x，维度y，步长stride，类型type
-function addPoolLayer(scene, dimensionX, dimensionY, stride = 0,  type = "max", axisPosition = 0) {
+function addPoolLayer(scene, dimensionX, dimensionY, stride = 0, type = "max", axisPosition = 0) {
     if (type !== "max" && type !== "mean") {
         console.log("unknown pooling type");
     } else {
@@ -111,9 +162,9 @@ function addPoolLayer(scene, dimensionX, dimensionY, stride = 0,  type = "max", 
         let material;
 
         if (type === "max")
-            material = new THREE.MeshBasicMaterial({color: 0xE9967A, side:THREE.DoubleSide});
+            material = new THREE.MeshBasicMaterial({color: 0xE9967A, side: THREE.DoubleSide});
         else
-            material = new THREE.MeshBasicMaterial({color: 0x2E8B57, side:THREE.DoubleSide});
+            material = new THREE.MeshBasicMaterial({color: 0x2E8B57, side: THREE.DoubleSide});
 
         let pool_count = dimensionX * dimensionY;
         const division = 7;
@@ -126,6 +177,8 @@ function addPoolLayer(scene, dimensionX, dimensionY, stride = 0,  type = "max", 
         pool_mesh.name = ""
         scene.add(pool_mesh);
 
+        //建立位置缓存
+        pool_mesh.instancePosition = [];
 
         //实例化池化元
         let pool_node = new THREE.Object3D();
@@ -138,6 +191,8 @@ function addPoolLayer(scene, dimensionX, dimensionY, stride = 0,  type = "max", 
                 let position_y = -division * j + y_bias;
                 pool_node.position.set(position_x, position_y, axisPosition);
                 pool_node.updateMatrix();
+
+                pool_mesh.instancePosition.push(new THREE.Vector3(position_x, position_y, axisPosition));
                 pool_mesh.setMatrixAt(matrix_index++, pool_node.matrix);
             }
         }
@@ -150,10 +205,116 @@ function addPoolLayer(scene, dimensionX, dimensionY, stride = 0,  type = "max", 
         let helper = new THREE.Box3Helper(layer_box, 0x000000);
         scene.add(helper);
 
-        //mean green
+        //添加文本标识
+        let text_position = {
+            x: 0,
+            y: (dimensionY / 2 + 1) * division,
+            z: axisPosition
+        };
+
+        let text = {
+            title: "池化层" + " " + type,
+            text_dimension: String(dimensionX) + "*" + String(dimensionY)
+        }
+
+        //添加文字标识
+        addSpriteText(scene, text, text_position, 2);
+
+        return pool_mesh.id;
     }
 }
 
-//添加节点连线边,需要输入与前层的连接关系
+//添加文字精灵
+//需要的参数 scene，文本结构体，位置，颜色类型
+//text:{title:"cov", dimension:"4x4x3"}
+function addSpriteText(scene, text, text_position, text_type = -1) {
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
 
-export {addNeuralLayer, addConvLayer, addPoolLayer};
+    //解析文本结构体 长度 换行
+    console.log(text.title, text.text_dimension);
+    let max_length = Math.max(text.title.length, text.text_dimension.length);
+    canvas.width = max_length * 40 + 40;
+    canvas.height = 120;
+    //制作矩形
+
+    //解析类型确定背景和字体颜色
+    if (text_type === 0)
+        ctx.fillStyle = "rgba(65,105,225,0.6)";  //神经元层
+    else if (text_type === 1)
+        ctx.fillStyle = "rgba(60,179,113,0.6)";  //卷积层
+    else if (text_type === 2)
+        ctx.fillStyle = "rgba(240,128,128,0.6)";  //池化层
+    else
+        ctx.fillStyle = "rgba(255,165,0,0.6)";  //其他层
+
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = "#fff";
+    ctx.font = "40px Arial";
+    ctx.fillText(text.title, 20, 50);
+    ctx.fillStyle = "#696969"
+    ctx.fillText(text.text_dimension, 20, 100);
+
+
+    let url = canvas.toDataURL('image/png');
+    let texture = THREE.ImageUtils.loadTexture(url);
+
+    let spriteMaterial = new THREE.SpriteMaterial({map: texture});
+    let sprite = new THREE.Sprite(spriteMaterial);
+    sprite.position.set(text_position.x, text_position.y, text_position.z);
+    sprite.scale.set(12, 6, 1);
+    scene.add(sprite);
+}
+
+
+//添加节点连线边,需要输入与前层的连接关系
+//pre_layer为一个结构体
+// let pre_layer = {
+//     id:1,
+//     dimensionX:3,
+//     dimensionY:3
+// }
+//todo 添加连接关系
+function addConnection(scene, pre_layer, cur_layer) {
+    //todo type check
+    let layer1 = scene.getObjectById(pre_layer);
+    let layer2 = scene.getObjectById(cur_layer);
+
+    //全连接关系
+    // let layer1_x = pre_layer.dimensionX;
+    // let layer1_y = pre_layer.dimensionY;
+    //
+    // let layer2_x = pre_layer.dimensionX;
+    // let layer2_y = pre_layer.dimensionY;
+
+    let layer1_count = layer1.count;
+    let layer2_count = layer2.count;
+
+    let temp_point1 = new THREE.Object3D();
+    let temp_point2 = new THREE.Object3D();
+
+    let material = new THREE.LineBasicMaterial({color: 0x778899});
+    let geometry = new THREE.BufferGeometry();
+    let points = [];
+
+    // let line_mesh = new THREE.InstancedMesh()
+
+    let random_node = Math.floor(Math.random() * layer1_count);
+
+    for (let i = 0; i < layer2_count; i++) {
+        let node1_position = layer1.instancePosition[random_node];
+        let node2_position = layer2.instancePosition[i];
+
+        points.push(node1_position);
+        points.push(node2_position);
+
+        geometry.setFromPoints(points);
+        let line = new THREE.Line(geometry, material);
+        scene.add(line);
+    }
+
+
+}
+
+export {addNeuralLayer, addConvLayer, addPoolLayer, addConnection};
